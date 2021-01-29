91486477f904f9dd3129c47ed0e5ecb63bd2362b
Allanzheng <zhengqiaoyang@xiao100.com>
2021-01-29 20:51:55 +0800

    修改文件结构

--- process.py
+++ process.py
@@ -9,11 +9,11 @@ go = '.go'
 def file_name(file_dir):
 
     for root, dirs, files in os.walk(file_dir):
-            if os.path.dirname(root)== file_dir or os.path.dirname(os.path.dirname(root))== file_dir:
+            if (os.path.dirname(root)== file_dir or os.path.dirname(os.path.dirname(root))== file_dir) and not root.isdigit():
                 for i in files:
                     if i =='process.py' or i in dirs:
                         continue
-                    if i.endswith(py) and i.isalpha():
+                    if i.endswith(py) :
                             str = i.strip(py)
                             if str not in dirs:
                                 filedir = root + "\\" + str
@@ -22,7 +22,7 @@ def file_name(file_dir):
                             newpath = root+"\\"+str+"\\"+i
                             print(oldpath,newpath)
                             shutil.move(oldpath,newpath)
-                    if i.endswith(go)  and i.isalpha() :
+                    if i.endswith(go) :
                             str = i.strip(go)
                             if str not in dirs:
                                 filedir = root + "\\" + str
@@ -35,7 +35,6 @@ def file_name(file_dir):
 
 
 
-print( re.search('.py','5328. The K Weakest Rows in a Matrix.py'))
 path = sys.path[0]
 print(path)
 file_name(path)
